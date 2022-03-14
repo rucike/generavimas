8393d963c74552c0a999bf36c7b1c51991a87987
rutpla <rutpla@C406A5>
2022-03-14 08:56:31 +0200

    Add project files.

---
+++ generavimas/Class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generavimas
+{
+    internal class Passw
+    {
+        public string GetRandomPassword(int length)
+        {
+            const string chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?!@#$%^&";
+
+            StringBuilder sb = new StringBuilder();
+            Random rnd = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = rnd.Next(chars.Length);
+                sb.Append(chars[index]);
+            }
+
+            return sb.ToString();
+
+        }
+    }
+}
---
+++ generavimas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace generavimas
+{
+    public class Example
+    {
+        /*public static string GetRandomPassword(int length)
+        {
+            const string chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?!@#$%^&";
+
+            StringBuilder sb = new StringBuilder();
+            Random rnd = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = rnd.Next(chars.Length);
+                sb.Append(chars[index]);
+            }
+
+            return sb.ToString();
+        }*/
+
+        public static void Main()
+        {
+            int length = 10;
+            Passw obj = new Passw();
+            string password;
+            password = obj.GetRandomPassword(length);
+            Console.WriteLine(password);
+        }
+    }
+}
